6f851f82c7046d7c8c10c1cdb724dbf33bf52f82
sdmnny <sdmnny@gmail.com>
2023-10-23 17:42:58 -0400

    finalized login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,42 @@ import React, { UseState, useState } from 'react';
 
 function Login() { 
   const [email, setEmail] = useState(''); 
-  
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.log('Email:', email);
+    console.log('Password:', password);
+  }; 
+
+  return (
+    <div>
+    <h1>Login Page</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+
+  );
+
 }
 
-export default Login; // This makes the Login component importable in other files
+export default Login; 
