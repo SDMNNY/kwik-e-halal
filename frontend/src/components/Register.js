8d71ce001e87971d86a9d335909686a5436ea460
sdmnny <sdmnny@gmail.com>
2023-10-24 14:25:27 -0400

    finihed register.js file

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -5,6 +5,50 @@ const [email, setEmail] = useState('');
 const [password, setPassword] = useState(''); 
 const [confirmPassword, setConfirmPassword] = useState(''); 
 
+const handleSubmit = (e) => { 
+  e.preventDefault(); 
+  if (password !== confirmPassword) {
+    alert('Passwords do not match!');
+    return;
+  }
+  console.log('Email:', email);
+  console.log('Password:', password);
+};
+return (
+  <div>
+    <h1>Register Page</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Confirm Password:</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  </div>
+);
 
 }
 
