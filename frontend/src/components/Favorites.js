fe95858c2e2d559f1a73b6278efa51a1d3cec1b8
sdmnny <sdmnny@gmail.com>
2023-10-31 17:08:47 -0400

    favorite.js completed

--- frontend/src/components/Favorites.js
+++ frontend/src/components/Favorites.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import RestaurantCard from './RestaurantCard';
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]); 
+
+  useEffect(() => {
+    // Here, you would call a function to fetch the user's favorite restaurants
+    // and update the `favorites` state
+  }, []);
+
   return (
     <div>
       <h1>Favorites Page</h1>
-      {/* This is where your list of favorite restaurants will go */}
+      <div>
+        {favorites.map((favorite) => (
+          <RestaurantCard key={favorite.id} restaurant={favorite} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default Favorites; // This makes the Favorites component importable in other files
+export default Favorites;
