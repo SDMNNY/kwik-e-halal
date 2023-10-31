1d04dfa0b2f29c26ecd7055c0a43df84214d8b5f
sdmnny <sdmnny@gmail.com>
2023-10-31 17:05:43 -0400

    completed search.js

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react';
-import RestaurantCard from './RestaurantCard'; 
+import RestaurantCard from './RestaurantCard';
 
-function search() { 
-  const [searchQuery, setSearchQuery] = useState(''); 
+function Search() {
+  const [searchQuery, setSearchQuery] = useState('');
   const [restaurants, setRestaurants] = useState([]); 
 
-  
-}
+  const handleSearch = () => {
+    console.log('Search Query:', searchQuery);
+    // Here, you would call a function to fetch restaurants based on the search query
+    // and update the `restaurants` state
+  };
 
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <div>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search for restaurants..."
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div>
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Search;
